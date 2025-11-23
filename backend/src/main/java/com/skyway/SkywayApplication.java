805fffc14f2dcc238a589e509ede9094a252174f
dmitriiserov <dmitriiserov2005@gmail.com>
2025-11-23 11:04:20 +0300

    init structure for backend

---
+++ backend/src/main/java/com/skyway/SkywayApplication.java
@@ -0,0 +1,13 @@
+package com.skyway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SkywayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SkywayApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/skyway/SkywayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.skyway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SkywayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
