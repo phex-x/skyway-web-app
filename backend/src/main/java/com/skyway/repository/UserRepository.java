51a49a269cea8b7ab4a62cb3181448476551d491
dmitriiserov <dmitriiserov2005@gmail.com>
2025-11-23 13:01:31 +0300

    created repository for user

---
+++ backend/src/main/java/com/skyway/repository/UserRepository.java
@@ -0,0 +1,12 @@
+package com.skyway.repository;
+
+import com.skyway.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Long> {
+    Optional<User> findByEmail(String email);
+}
