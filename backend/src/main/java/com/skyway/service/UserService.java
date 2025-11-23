75bb98010c622dcc647c41eab722bef0211aafaf
dmitriiserov <dmitriiserov2005@gmail.com>
2025-11-23 13:02:03 +0300

    created service for user

---
+++ backend/src/main/java/com/skyway/service/UserService.java
@@ -0,0 +1,60 @@
+package com.skyway.service;
+
+import com.skyway.dto.UserCreateDTO;
+import com.skyway.dto.UserResponseDTO;
+import com.skyway.entity.User;
+import com.skyway.error.UserAlreadyExistsError;
+import com.skyway.error.UserNotFoundError;
+import com.skyway.mapper.UserMapper;
+import com.skyway.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserService {
+    @Autowired
+    private UserRepository userRepository;
+
+    private UserMapper userMapper;
+
+    public UserResponseDTO createUser(UserCreateDTO userCreateDTO) {
+        if (userRepository.findByEmail(userCreateDTO.getEmail()).isPresent()) {
+            throw new UserAlreadyExistsError("user with email " + userCreateDTO.getEmail() + " already exists");
+        }
+        User user = userMapper.toUser(userCreateDTO);
+        user = userRepository.save(user);
+        return userMapper.toUserResponseDTO(user);
+    }
+
+    public UserResponseDTO getUserByEmail(String email) {
+        User user = userRepository.findByEmail(email)
+                .orElseThrow(() -> new UserNotFoundError("user with email " + email + " not found"));
+        return userMapper.toUserResponseDTO(user);
+    }
+
+    public UserResponseDTO getUserById(Long id) {
+        User user = userRepository.findById(id)
+                .orElseThrow(() -> new UserNotFoundError("user with id: " + id + " not found"));
+        return userMapper.toUserResponseDTO(user);
+    }
+
+    public void deleteUserById(Long id) {
+        User user = userRepository.findById(id)
+                .orElseThrow(() -> new UserNotFoundError("user with id: " + id + " not found"));
+        userRepository.delete(user);
+    }
+
+    public void enableUserById(Long id) {
+        User user = userRepository.findById(id)
+                .orElseThrow(() -> new UserNotFoundError("user with id: " + id + " not found"));
+        user.setIsEnabled(true);
+        userRepository.save(user);
+    }
+
+    public void disableUserById(Long id) {
+        User user = userRepository.findById(id)
+                .orElseThrow(() -> new UserNotFoundError("user with id: " + id + " not found"));
+        user.setIsEnabled(false);
+    }
+
+}
