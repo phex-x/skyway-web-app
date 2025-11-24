3ef0b3f1fa1820a94c0f1f36b111f8a1f7ced1c6
dmitriiserov <dmitriiserov2005@gmail.com>
2025-11-24 09:20:52 +0300

    added implementation of user details

--- backend/src/main/java/com/skyway/entity/User.java
+++ backend/src/main/java/com/skyway/entity/User.java
@@ -55,6 +55,48 @@ public class User implements UserDetails {
     @Column(name = "updated_at")
     private LocalDateTime updatedAt;
 
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        Set<GrantedAuthority> authorities = new HashSet<>();
+        authorities.add(new SimpleGrantedAuthority("ROLE_" + role.name()));
+        switch (role) {
+            case ADMIN:
+                authorities.add(new SimpleGrantedAuthority("ROLE_ADMIN"));
+                authorities.add(new SimpleGrantedAuthority("ROLE_USER"));
+                authorities.add(new SimpleGrantedAuthority("ROLE_STAFF"));
+                break;
+            case STAFF:
+                authorities.add(new SimpleGrantedAuthority("ROLE_STAFF"));
+                authorities.add(new SimpleGrantedAuthority("ROLE_USER"));
+                break;
+            case USER:
+                authorities.add(new SimpleGrantedAuthority("ROLE_USER"));
+        }
+        return authorities;
+    }
+
+    @Override
+    public @Nullable String getPassword() {
+        return this.hashedPassword;
+    }
+
+    @Override
+    public String getUsername() {
+        return this.email;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {return true;}
+
+    @Override
+    public boolean isAccountNonLocked() {return true;}
+
+    @Override
+    public boolean isCredentialsNonExpired() {return true;}
+
+    @Override
+    public boolean isEnabled() {return Boolean.TRUE.equals(isEnabled);}
+
     //getters
     public Long getId() {return this.id;}
     public String getEmail() {return this.email;}
@@ -68,6 +110,7 @@ public class User implements UserDetails {
     public LocalDateTime getUpdatedAt() {return this.updatedAt;}
     public Role getRole() {return this.role;}
     public Boolean getIsEnabled() {return this.isEnabled;}
+    public String getHashedPassword(String password) {return this.hashedPassword;}
 
     //setters
     public void setId(Long id) {this.id = id;}
