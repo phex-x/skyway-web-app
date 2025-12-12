38b8a7e222fb0c05538bc2006e8c0d67bf5b4856
dmitriiserov <dmitriiserov2005@gmail.com>
2025-12-12 18:00:12 +0300

    created exist check by reg number

--- backend/src/main/java/com/skyway/repository/AirplaneRepository.java
+++ backend/src/main/java/com/skyway/repository/AirplaneRepository.java
@@ -6,4 +6,5 @@ import org.springframework.stereotype.Repository;
 
 @Repository
 public interface AirplaneRepository extends JpaRepository<Airplane, Long> {
+    Boolean existsByRegistrationNumber(String registrationNumber);
 }
