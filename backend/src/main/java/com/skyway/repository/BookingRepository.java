53d0ecfc03c3a9dc4190db6635a8ff2971134382
dmitriiserov <dmitriiserov2005@gmail.com>
2025-12-12 18:25:54 +0300

    created method for getting all and finding by reference

--- backend/src/main/java/com/skyway/repository/BookingRepository.java
+++ backend/src/main/java/com/skyway/repository/BookingRepository.java
@@ -4,6 +4,10 @@ import com.skyway.entity.Booking;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface BookingRepository extends JpaRepository<Booking, Long> {
+    public boolean existsByBookingReference(String bookingReference);
+    public List<Booking> findBookingsByUserId(Long userId);
 }
