b9bde409f4929b3a90ac76bf35c6aff7afe2c3d4
dmitriiserov <dmitriiserov2005@gmail.com>
2025-12-13 19:41:39 +0300

    add routing

--- frontend/skyway-app/src/App.js
+++ frontend/skyway-app/src/App.js
@@ -3,6 +3,11 @@ import { AuthProvider } from './context/AuthContext';
 import HomePage from './pages/HomePage';
 import RegistrationPage from './pages/RegistrationPage';
 import LoginPage from './pages/LoginPage';
+import FlightSearchPage from './pages/FlightSearchPage';
+import BookingPage from './pages/BookingPage';
+import ManageBookingPage from './pages/ManageBookingPage';
+import ProfilePage from './pages/ProfilePage';
+import BookingSearchPage from './pages/BookingSearchPage';
 
 const AppContent = () => {
   return (
@@ -11,6 +16,11 @@ const AppContent = () => {
         <Route path="/" element={<HomePage />} />
         <Route path="/register" element={<RegistrationPage />} />
         <Route path="/login" element={<LoginPage />} />
+        <Route path="/booking-search" element={<BookingSearchPage />} />
+        <Route path="/flights" element={<FlightSearchPage />} />
+        <Route path="/booking" element={<BookingPage />} />
+        <Route path="/manage-booking" element={<ManageBookingPage />} />
+        <Route path="/profile" element={<ProfilePage />} />
       </Routes>
     </Router>
   );
