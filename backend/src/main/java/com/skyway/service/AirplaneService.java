ba7cdfa1b87c187eb7586b6ddf82ff81f217d46c
dmitriiserov <dmitriiserov2005@gmail.com>
2025-12-13 20:23:18 +0300

    added method exist and changed return to dto

--- backend/src/main/java/com/skyway/service/AirplaneService.java
+++ backend/src/main/java/com/skyway/service/AirplaneService.java
@@ -1,22 +1,29 @@
 package com.skyway.service;
 
+import com.skyway.dto.AirplaneCreate;
 import com.skyway.entity.Airplane;
 import com.skyway.error.AirplaneAlreadyExistsException;
 import com.skyway.error.AirplaneNotFoundException;
+import com.skyway.mapper.AirplaneMapper;
 import com.skyway.repository.AirplaneRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public class AirplaneService {
     @Autowired
     private AirplaneRepository airplaneRepository;
 
-    public void createAirplane(Airplane airplane){
-        if (!airplaneRepository.existsByRegistrationNumber(airplane.getRegistrationNumber())) {
-            airplaneRepository.save(airplane);
+    @Autowired
+    private AirplaneMapper airplaneMapper;
+
+    public void createAirplane(AirplaneCreate airplaneCreate) throws AirplaneAlreadyExistsException {
+        if (!airplaneRepository.existsByRegistrationNumber(airplaneCreate.getRegistrationNumber())) {
+            airplaneRepository.save(airplaneMapper.toAirplane(airplaneCreate));
         } else throw new AirplaneAlreadyExistsException("airplane with registration number: " +
-                airplane.getRegistrationNumber() + " already exists");
+                airplaneCreate.getRegistrationNumber() + " already exists");
     }
 
     public Airplane getAirplaneById(Long id){
@@ -30,5 +37,13 @@ public class AirplaneService {
             throw new AirplaneNotFoundException("airplane with id: " + id + " doecn't exists");
         }
         airplaneRepository.deleteById(id);
+    }
+
+    public boolean existsByCode(String code) {
+        return airplaneRepository.existsByRegistrationNumber(code);
+    }
+
+    public List<Airplane> getAllAirplanes(){
+        return airplaneRepository.findAll();
     }
 }
