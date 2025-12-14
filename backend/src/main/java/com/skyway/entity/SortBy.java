0ea2d33fb7a697fce861cbe7feacb540cf92a9c8
dmitriiserov <dmitriiserov2005@gmail.com>
2025-12-14 23:56:28 +0300

    created enum

---
+++ backend/src/main/java/com/skyway/entity/SortBy.java
@@ -0,0 +1,8 @@
+package com.skyway.entity;
+
+public enum SortBy {
+    PRICE_ASC,      // по цене (сначала дешевые)
+    PRICE_DESC,     // по цене (сначала дорогие)
+    DEPARTURE_ASC,  // по времени вылета (сначала ранние)
+    DEPARTURE_DESC
+}
