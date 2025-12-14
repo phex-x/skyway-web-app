c38625786e8d1d9815cb5c1a8d11c97da446d211
dmitriiserov <dmitriiserov2005@gmail.com>
2025-12-14 23:58:18 +0300

    add pageable

--- backend/src/main/java/com/skyway/repository/FlightRepository.java
+++ backend/src/main/java/com/skyway/repository/FlightRepository.java
@@ -2,33 +2,38 @@ package com.skyway.repository;
 
 import com.skyway.entity.Flight;
 import com.skyway.entity.SeatClass;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.time.LocalDate;
-import java.util.List;
 
 @Repository
 public interface FlightRepository extends JpaRepository<Flight, Long> {
-    @Query("SELECT f FROM Flight f " +
-            "JOIN f.departureAirport da " +
-            "JOIN f.arrivalAirport aa " +
-            "WHERE da.city = :departureCity " +
-            "AND aa.city = :arrivalCity " +
-            "AND DATE(f.scheduledDeparture) = :departureDate " +
-            "AND ( " +
-            "   (:seatClass = 'ECONOMY' AND f.remainingEconomySeats >= :passengers) OR " +
-            "   (:seatClass = 'BUSINESS' AND f.remainingBusinessSeats >= :passengers) " +
-            ")")
-    List<Flight> flightSearch(
-            @Param("departureCity") String departureCity,
-            @Param("arrivalCity") String arrivalCity,
+    @Query("""
+        SELECT f FROM Flight f
+        WHERE LOWER(f.departureAirport.city) LIKE LOWER(CONCAT('%', :departureAirport, '%'))
+        AND LOWER(f.arrivalAirport.city) LIKE LOWER(CONCAT('%', :arrivalAirport, '%'))
+        AND DATE(f.scheduledDeparture) = :departureDate
+        AND (:passengerCount <=
+             CASE :seatClass
+                WHEN 'ECONOMY' THEN f.remainingEconomySeats
+                WHEN 'BUSINESS' THEN f.remainingBusinessSeats
+                ELSE 0
+             END)
+        """)
+    Page<Flight> flightSearch(
+            @Param("departureAirport") String departureAirport,
+            @Param("arrivalAirport") String arrivalAirport,
             @Param("departureDate") LocalDate departureDate,
-            @Param("passengers") int passengers,
-            @Param("seatClass") String seatClass
+            @Param("passengerCount") int passengerCount,
+            @Param("seatClass") String seatClass,
+            Pageable pageable
     );
 
-    public boolean existsByFlightNumber(String flightNumber);
+    boolean existsByFlightNumber(String flightNumber);
+    Page<Flight> findAll(Pageable pageable);
 }
